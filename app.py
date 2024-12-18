5070534e9239b2f7a85a139ffbcab21d78b554e7
Ivan <148480081+ivargas58@users.noreply.github.com>
2024-12-18 10:55:40 -0400

    Update app.py if main 2

--- app.py
+++ app.py
@@ -175,5 +175,4 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
-
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
